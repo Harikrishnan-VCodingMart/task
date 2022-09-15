4170aa19fecf9e64be271fddab0d3a0d7bbdefef
harikrishnan.v <harikrishnan.v@codingmart.com>
2022-09-15 12:56:40 +0530

    added ecosysr pm2 deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,8 +12,8 @@ module.exports = {
       user : 'balaji',
       host : '192.168.1.140',
       ref  : 'origin/master',
-      repo : 'git@github.com/Harikrishnan-VCodingMart/task',
-      path : '/opt/deployment/nodeserver',
+      repo : 'git@github.com/Harikrishnan-VCodingMart/task.git',
+      path : '/home/balaji/Documents/sep_14_task/3/task',
       'post-deploy' : 'npm install && pm2 start index.js',
     }
   }
